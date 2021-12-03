e24c8a85f44f6038e2ffbb1ac78f83ef0eb8b43e
Guilherme R <guilhaxr123@gmail.com>
2021-12-03 23:37:17 +0000

    other kata

--- Katalvl6/FindTheOddInt.js
+++ Katalvl6/FindTheOddInt.js
@@ -10,47 +10,42 @@
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
 
-function findOdd(A) {
-   let counter= 0;
+// function findOdd(A) {
+//    let counter= 0;
     
-    const NumInt =  A.sort((a, b)=> a - b);
-    console.log(NumInt);
-    for( let i = 0; i < NumInt.length; i++){
-        for(let k = 0; k < NumInt.length; k++){
-            if( NumInt[i] === NumInt[k]){
-                counter++
-            }
-        }
-        if(console.log(counter) % 2 !== 0){
-            return NumInt[i]
+//     const NumInt =  A.sort((a, b)=> a - b);
+//     console.log(NumInt);
+//     for( let i = 0; i < NumInt.length; i++){
+//         for(let k = 0; k < NumInt.length; k++){
+//             if( NumInt[i] === NumInt[k]){
+//                 counter++
+//             }
+//         }
+//         if(console.log(counter) % 2 !== 0){
+//             return NumInt[i]
+//         }
+//     }
+ 
+// }
+//   findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])
+
+function findOdd(A) {
+    
+    let counter = {};
+
+   const NumInt =  A.sort((a, b)=> a - b)
+
+    NumInt.forEach((e)=>{counter[e] = (counter[e] || 0) +1})
+    console.log(counter)
+
+    for( const key in counter){
+        if(counter[key] % 2 !==0){
+            return console.log(Number(key))
         }
     }
- 
-}
+  }
+
   findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])
-
-// function findOdd(A) {
-    
-//     let counter = {};
-//     let finalResult;
-
-//    const NumInt =  A.sort((a, b)=> a - b)
-
-//     NumInt.forEach((e)=>{counter[e] = (counter[e] || 0) +1})
-//     console.log(counter)
-
-//     const values = Object.values(counter).filter((value)=>{
-//         return value % 2 !== 0
-//     })
-
-//     Object.values(counter).filter((value)=>{
-//        if(value % 2 !== 0){
-//            return console.log(value)
-//        }
-//     })
-//   }
-
-//   findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])
 
 // function aaa(n){
 //     var count = {};
---
+++ Katalvl6/Stop-gninnipS-My-sdroW.js
@@ -0,0 +1,23 @@
+// Write a function that takes in a string of one or more words, and returns the same string,
+//  but with all five or more letter words reversed (Just like the name of this Kata).
+//   Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+function spinWords(string){
+    let counter = 0;
+    let newArr = string.split(" ");
+    
+  
+   for( let i = 0; i < newArr.length; i++){
+    if(newArr[i].length >= 5){
+        console.log(newArr.join(" ").split("").reverse())
+    }
+  
+   }
+    
+  }
+
+  spinWords("Hey fellow warriors")
